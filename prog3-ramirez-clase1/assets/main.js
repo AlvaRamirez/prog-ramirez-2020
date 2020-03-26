d95225450cab9965c6a099b403da8974f8ea1025
AlvaRamirez <55244792+AlvaRamirez@users.noreply.github.com>
2020-03-26 11:42:49 -0300

    asd

---
+++ prog3-ramirez-clase1/assets/main.js
@@ -0,0 +1,36 @@
+$( document ).ready(function(){ 
+  
+
+    $('#handle_ajax').click(function(){
+        
+    $.ajax({
+        url: "https://reqres.in/api/users",
+        success: function (response){
+            console.log(response.data);
+            var listadoUsuarios= $('#listado');
+
+            $.each(response.data,function(index, elemento){
+                console.log(elemento.fist_name);
+                listadoUsuarios.append('<div class="card"><h2>'
+                 + elemento.first_name + 
+                 '</h2><img src="'+ elemento.avatar +'"></div>');
+            })
+        },
+
+        error: function(e){
+            console.log('CUIDADO BRO', e)
+        },
+
+        beforeSend: function () { $("#loader").show();
+        console.log('buscando data')},           
+        complete: function () { $("#loader").hide(); 
+        console.log('data buscada!') }
+        
+    });
+    });
+
+    $('#handle_ajax_delete').click(function(){
+       $('#listado').html ('')
+    });
+
+});

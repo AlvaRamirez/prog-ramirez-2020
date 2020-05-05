ab8990d458d2884eb7cc9cd212de116df0e31b1b
AlvaRamirez <55244792+AlvaRamirez@users.noreply.github.com>
2020-05-05 17:39:42 -0300

    .

--- hola-express/models/heladerias.js
+++ hola-express/models/heladerias.js
@@ -1,11 +1,16 @@
-var mongoose = require ('mongoose');
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var heladieriasSchema= new Schema({
+var heladieriasSchema = new Schema({
     id:{type: String},
     lat:{type: String},
+    lng:{type: String},
     name:{type: String},
     description:{type: String},
-    
+    type: {type: String},
+    web: {type:String},
+    img: {type:String}
 
-})
+});
+
+module.exports = mongoose.model('Heladeria', heladieriasSchema, 'heladerias' );

af58626e5da81e9d7fb78226c8d728131c409a7b
AlvaRamirez <55244792+AlvaRamirez@users.noreply.github.com>
2020-05-14 11:04:58 -0300

    subida

--- hola-express/routes/heladerias.js
+++ hola-express/routes/heladerias.js
@@ -1,165 +1,129 @@
-const crudHeladerias = (app) =>{
-    const Heladerias = require('../models/heladerias.js')
-}
+const crudHeladerias = (app) => {
+    const Heladeria = require('../models/heladerias.js')
 
-findAllCervecerias = (req,res)=> {
-    Heladerias.find((err, heladerias)=>{
-        if(!err){
-            console.log('GET /cervecerias');
-            res.send(heladerias)
-        }
-    })
-}
 
-export default crudHeladerias;
+    findAllHeladerias = (req, res) => {
+        console.log('entro en heladerias');
+        Heladeria.find((err, heladerias) => {
+            console.log('error', err);
+            if (!err) {
+                console.log('GET /heladerias');
+                res.send(heladerias)
+            }
 
-//GET - Get all registers from the DB
-findAllPeliculas = (req, res) => {
+        })
 
-    Heladeria.find((err, heladerias) => {
-    
-    if (!err) {
-    
-    console.log('GET /heladerias')
-    
-    res.send(heladerias);
-    
-    } else {
-    
-    console.log('ERROR: ' + err);
-    
     }
-    
-    });
-    
-    };
-    
-    
-    
-    //POST - Insert a new register in the DB
+
     addHeladeria = function (req, res) {
+
+        console.log('POST');
     
-    console.log('POST');
+        console.log(req.body);
     
-    console.log(req.body);
+        var heladeria = new Heladeria({
     
+            id: req.body.id,
+            lat: req.body.lat,
+            lng: req.body.lng,
+            name: req.body.name,
+            description: req.body.description,
+            type: req.body.type,
+            web: req.body.web,
+            img: req.body.img
     
+        });
     
-    var heladeria = new Heladeria({
-    
-    id: req.body.id,
-    
-    slug: req.body.slug,
-    
-    nombre: req.body.nombre,
-    
-    lanzamiento: req.body.lanzamiento,
-    
-    cover: req.body.cover,
-    
-    actores: req.body.actores
-    
-    });
-    
-    
-    
-    heladeria.save(function (err) {
-    
-    if (!err) {
-    
-    console.log('Created');
-    
-    } else {
-    
-    console.log('ERROR: ' + err);
-    
-    }
-    
-    });
-    
-    
-    
-    res.send(heladeria);
-    
-    };
-    
-    
-    
-    //PUT - Update a register already exists in the DB
-    modificarPeliculas = function (req, res) {
-    
-    heladeria.findById(req.params.id, function (err, heladeria) {
-    
-        heladeria.id = req.body.id;
-    
-        heladeria.slug = req.body.slug;
-    
-        heladeria.nombre = req.body.nombre;
-    
-        heladeria.lanzamiento = req.body.lanzamiento;
-    
-        heladeria.cover = req.body.cover;
-    
-        heladeria.actores = req.body.actores;
     
         heladeria.save(function (err) {
     
-    if (!err) {
+            if (!err) {
     
-    console.log('Updated');
+                console.log('Created');
     
-    } else {
+            } else {
     
-    console.log('ERROR: ' + err);
+                console.log('ERROR: ' + err);
+    
+            }
+    
+        });
+    
+        res.send(heladeria);
+    
+    };
+
+    modificarHeladerias = function (req, res) {
+
+        heladeria.findById(req.params.id, function (err, heladeria) {
+    
+            heladeria.id = req.body.id;
+            heladeria.lat = req.body.lat;
+            heladeria.lng = req.body.lng;
+            heladeria.name = req.body.name;
+            heladeria.description = req.body.description;
+            heladeria.type = req.body.type;
+            heladeria.web = req.body.web;
+            heladeria.img = req.body.img;
+            heladeria.save(function (err) {
+    
+                if (!err) {
+    
+                    console.log('Updated');
+    
+                } else {
+    
+                    console.log('ERROR: ' + err);
+    
+                }
+    
+                res.send(heladeria);
+    
+            });
+    
+        });
     
     }
-    
-    res.send(heladeria);
-    
-    });
-    
-    });
-    
-    }
-    
-    
-    
-    
-    
-    //DELETE - Delete a register with specified ID
+
     deleteHeladeria = function (req, res) {
-    
+
         Heladeria.findById(req.params.id, function (err, heladeria) {
     
             heladeria.remove(function (err) {
     
-    if (!err) {
+                if (!err) {
     
-    console.log('Removed');
+                    console.log('Removed');
     
-    } else {
+                } else {
     
-    console.log('ERROR: ' + err);
+                    console.log('ERROR: ' + err);
+    
+                }
+    
+                res.send(heladeria);
+    
+            })
+    
+        });
     
     }
-    
-    res.send(heladeria);
-    
-    })
-    
-    });
-    
-    }
-    
-    
-    
-    //Rutas de la API, asociadas a una función
-    
     
     app.get('/heladerias', findAllHeladerias);
-    
+
     app.post('/heladerias', addHeladeria);
-    
+         
     app.put('/heladerias/:id', modificarHeladerias);
-    
+         
     app.delete('/heladerias/:id', deleteHeladeria);
+}
+
+module.exports = crudHeladerias;
+
+
+
+
+
+
+
+

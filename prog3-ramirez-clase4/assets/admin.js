9f01ed706c0b7a26a7ff09b56b140a81ec6e1419
AlvaRamirez <55244792+AlvaRamirez@users.noreply.github.com>
2020-05-26 22:16:50 -0300

    neew

--- prog3-ramirez-clase4/assets/admin.js
+++ prog3-ramirez-clase4/assets/admin.js
@@ -1,6 +1,39 @@
+//Elementos del DOM
 const $listado = document.querySelector('#listado');
+const $form_field_lat = document.querySelector('#form_field_lat');
+const $form_field_lng = document.querySelector('#form_field_lng');
+const $form_field_name = document.querySelector('#form_field_name');
+const $form_field_description = document.querySelector('#form_field_description');
+const $form_field_type = document.querySelector('#form_field_type');
+const $form_field_id = document.querySelector('#form_field_id'); //Se agregó el campo ID como input hidden
+const $form_main = document.querySelector('#form_main');
 
 
+
+
+const getHeladerias = async (id = '') => {
+    const result = await api.getHeladerias();
+    console.log(result)
+    if (id == '') {
+        $listado.innerHTML = '';
+        result.forEach(element => {
+            $listado.innerHTML += dataRow(element)
+        });
+        //Agrego estos dos nuevo EventListeners apenas agrego los elementos nuevos al DOM
+        const $btnsDelete = document.querySelectorAll('.handleDelete');
+        $btnsDelete.forEach(element => {
+            element.addEventListener('click', handleClickDelete)
+        });
+        const $btnsEdit = document.querySelectorAll('.handleEdit');
+        $btnsEdit.forEach(element => {
+            element.addEventListener('click', handleClickEdit)
+        });
+    } else {
+        const elementByID = result.find(el => id == el._id)
+        return elementByID
+    }
+
+}
 
 const dataRow = props => {
 
@@ -21,23 +54,6 @@ const dataRow = props => {
     `
 }
 
-const getHeladerias = async (id = '') => {
-
-    const result = await api.getHeladerias();
-    console.log(result)
-    if (id == '') {
-        $listado.innerHTML = '';
-        result.forEach(element => {
-            $listado.innerHTML += dataRow(element)
-        });
-
-    } else {
-        const elementByID = result.find(el => id == el._id)
-        return elementByID
-    }
-
-}
-
 getHeladerias();
 
 
@@ -49,7 +65,24 @@ const deleteHeladeria = async (id) => {
 
 }
 
-const completeForm = (reg) => { 
+const handleClickDelete = async () => {
+    const id = event.target.dataset.id;
+    deleteHeladeria(id);
+}
+
+//UPDATE
+const updateHeladeria = async (data,id) => {
+    const result = await api.updateHeladeria(data,id);
+    console.log('Updated', result)
+    getHeladerias();
+}
+const handleClickEdit = async () => {
+    const id = event.target.dataset.id;
+    const reg = await getHeladerias(id);
+    completeForm(reg)
+}
+
+const completeForm = (reg) => {
     const { lat, lng, name, description, type } = reg;
 
     $form_field_lat.value = lat
@@ -78,3 +111,28 @@ document.addEventListener('click', async function () {
 
 }, false)
 
+//CREATE
+const createHeladeria = async (data) => {
+    const result = await api.createHeladeria(data);
+    console.log('Created',result)
+    getHeladerias();
+}
+
+//FORM (Update o Create)
+$form_main.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const id = $form_field_id.value
+    const formData = {
+        "lat": $form_field_lat.value,
+        "lng": $form_field_lng.value,
+        "name": $form_field_name.value,
+        "description": $form_field_description.value,
+        "type": $form_field_type.value
+    }
+    updateHeladeria(formData,id);
+    
+
+    //Reseteo el form
+    $form_field_id.value = '';
+    $form_main.reset();
+})
--- prog3-ramirez-clase4/utils/api.js
+++ prog3-ramirez-clase4/utils/api.js
@@ -1,5 +1,5 @@
-const baseUrl = 'http://localhost:3000/';
-
+//const baseUrl = 'http://localhost:3000/';
+const baseUrl = 'https://fmap.now.sh/';
 const apiHeaders= {
 
     'Content-Type': 'application/json',
@@ -16,7 +16,7 @@ const fetchParams = (method, data = '') => {
         ...body
     }
 }
-
+/*
 const api = {
 
     //GET
@@ -38,4 +38,32 @@ const api = {
 
     //POST
 
+}*/
+
+const api = {
+    //Funciones CRUD
+    //CREATE (To be continued...)
+    createCervecerias: async formData => {
+        const dataResponse = await fetch(baseUrl + 'cervecerias', fetchParams('POST', formData));
+        const dataInfo = await dataResponse.json();
+        return dataInfo;
+    },
+    //READ
+    getCervecerias: async () => {
+        const dataResponse = await fetch(baseUrl + 'cervecerias', fetchParams('GET'));
+        const dataInfo = await dataResponse.json();
+        return dataInfo;
+    },
+    //UPDATE
+    updateCervecerias: async (formData,id) => {
+        const dataResponse = await fetch(baseUrl + 'cerveceria/' + id, fetchParams('PUT', formData));
+        const dataInfo = await dataResponse.json();
+        return dataInfo;
+    },
+    //DELETE
+    deleteCervecerias: async id => {
+        const dataResponse = await fetch(baseUrl + 'cerveceria/' + id, fetchParams('DELETE'));
+        const dataInfo = await dataResponse.json();
+        return dataInfo;
+    },
 }

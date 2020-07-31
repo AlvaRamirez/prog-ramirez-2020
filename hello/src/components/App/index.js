0387f5378122ec0a1ae4f2ceb24f0f75f4800483
AlvaRamirez <55244792+AlvaRamirez@users.noreply.github.com>
2020-07-31 16:59:26 -0300

    esoo

--- hello/src/components/App/index.js
+++ hello/src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import '../App/index.css';
 import Header from '../Header';
 import Footer from '../Footer';
@@ -47,6 +47,237 @@ class App extends React.Component {
                 <Footer texto="Este es mi footer horrible" fecha={this.state.date} />
             </div>)
     }
+}
+
+export default App;*/
+
+import React from 'react';
+import '../App/index.css';
+import faker from 'faker'
+import EmployeeCard from '../EmployeeCard'
+import Form from '../Form'
+
+
+class App extends React.Component {
+    constructor() {
+        super()
+        //faker.seed(123);
+       // faker.locale = "ja";
+        const employees = Array.from({ length: 30 }, () => ({
+            name: faker.name.findName(),
+            sector: faker.name.jobArea(),
+            avatar: faker.image.avatar(),
+            id: faker.random.uuid(),
+        }))
+        //NUEVO CLASE 2/3
+        const sectors = employees.map(({ sector }) => sector)
+        const sectorsUnrepeated = new Set(sectors)
+        const sectorsArray = [...sectorsUnrepeated]
+
+        this.state = {
+            employees: employees,
+            listBackup: employees,
+            empleadoDelMes: null,
+            employeeName: '',
+            sectors: sectorsArray,
+            dropdownActive: false,
+            selectedSector: '',
+            employeeToEdit:{}
+
+        }
+        this.handleEmpleadoMesClick = this.handleEmpleadoMesClick.bind(this) //Linea mounstrosa
+        this.handleAddEmployeeSubmit = this.handleAddEmployeeSubmit.bind(this)
+        this.handleAddEmployeeChange = this.handleAddEmployeeChange.bind(this)
+    }
+
+    handleEmpleadoMesClick(employeeId) {
+
+        //setear el estado diciendo cual es el id del empleado del mes
+        this.setState({
+            empleadoDelMes: employeeId
+        })
+        setTimeout(() => {
+            console.log('state', this.state.empleadoDelMes)
+
+        }, 1);
+    }
+
+    //NUEVO CLASE 2/3
+    handleAddEmployeeChange = event => {
+        const { value } = event.target
+        this.setState({ employeeName: value })
+    }
+    //NUEVO CLASE 2/3
+    handleAddEmployeeSubmit = event => {
+        event.preventDefault()
+        const { employees, employeeName } = this.state
+
+        const newEmployee = {
+            name: employeeName,
+            sector: faker.name.jobArea(),
+            avatar: faker.image.avatar(),
+            id: faker.random.uuid(),
+        }
+        const newList = [newEmployee, ...employees] //Linea importante!
+        this.setState({ employees: newList })
+    }
+
+    //NUEVO CLASE 2/3
+    //Dropdown
+    handleDropdownActive = () =>
+        this.setState(prevState => ({ dropdownActive: !prevState.dropdownActive }))
+
+    //NUEVO CLASE 2/3
+    handleSelectSector = sector => {
+        const { listBackup } = this.state
+
+        const listFilteredBySector = listBackup.filter(employee => employee.sector === sector)
+
+        this.setState({
+            selectedSector: sector,
+            employees: listFilteredBySector,
+            dropdownActive: false,
+        })
+    }
+    //NUEVO CLASE 2/3
+    handleRemoveSelectedSector = () => {
+        this.setState(prevState => ({ employees: prevState.listBackup, selectedSector: '' }))
+    }
+
+    handleRemoveEmployee = id => {
+        const { employees } = this.state
+        const listWithoutEmployee = employees.filter(employee => employee.id !== id)
+        this.setState({ employees: listWithoutEmployee })
+    }
+
+    render() {
+        const {
+            newEmployee,
+            employees,
+            idMonthEmployee,
+            dropdownActive,
+            sectors,
+            selectedSector,
+            modalActive,
+            employeeToEdit,
+        } = this.state
+
+        return (
+            <div className='container'>
+                <h1 className='is-size-1'>Lista de Empleados</h1>
+
+                <Form
+                    handleAddEmployeeChange={this.handleAddEmployeeChange}
+                    handleAddEmployeeSubmit={this.handleAddEmployeeSubmit}
+                    employeeName={this.state.employeeName}
+                />
+
+                <Dropdown
+                    sectors={sectors}
+                    dropdownActive={dropdownActive}
+                    selectedSector={selectedSector}
+                    onSelectSector={this.handleSelectSector}
+                    onDropdownActive={this.handleDropdownActive}
+                    onRemoveSelectedSector={this.handleRemoveSelectedSector}
+                />
+
+                {modalActive && (
+                    <div className='modal is-active'>
+                        <div className='modal-background' />
+                        <div className='modal-card'>
+                            <header className='modal-card-head'>
+                                <p className='modal-card-title'>Modal title</p>
+                                <button className='delete' aria-label='close' />
+                            </header>
+                            <section className='modal-card-body'>
+                                <form className='form-add-employee'>
+                                    <input
+                                        className='input'
+                                        type='text'
+                                    />
+                                </form>
+                            </section>
+                        </div>
+                    </div>
+                )}
+
+                {
+                    this.state.employees.map((employee) =>
+                        <EmployeeCard
+                            employeeData={employee}
+                            key={employee.id}
+                            handleEmpleadoMesClick={this.handleEmpleadoMesClick}
+                            empleadoDelMesID={this.state.empleadoDelMes}
+                            handleRemoveEmployee={this.handleRemoveEmployee}
+                        />
+                    )
+                }
+            </div>
+        )
+    }
+}
+
+const Dropdown = props => {
+
+    const {
+        dropdownActive,
+        sectors,
+        onDropdownActive,
+        onSelectSector,
+        selectedSector,
+        onRemoveSelectedSector,
+    } = props
+
+    return (
+        <div>
+            <div className={`dropdown ${dropdownActive === true ? 'is-active' : ''}`}>
+                <div className='dropdown-trigger'>
+                    <button
+                        className='button'
+                        aria-haspopup='true'
+                        aria-controls='dropdown-menu'
+                        onClick={onDropdownActive}
+                    >
+                        <span>Elegir sector</span>
+                        <span className='icon is-small'>
+                            <i className='fas fa-angle-down' aria-hidden='true' />
+                        </span>
+                    </button>
+                </div>
+                <div className='dropdown-menu' id='dropdown-menu' role='menu'>
+                    <div className='dropdown-content'>
+                        {sectors.map(sector => (
+                            <a
+                                key={sector}
+                                href='#'
+                                className='dropdown-item'
+                                onClick={() => onSelectSector(sector)}
+                            >
+                                {sector}
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </div>
+
+
+            {selectedSector && (
+                <button
+                    className='button'
+                    aria-haspopup='true'
+                    aria-controls='dropdown-menu'
+                    onClick={onRemoveSelectedSector}
+                    style={{ marginLeft: '15px' }}
+                >
+                    <span>{selectedSector}</span>
+                    <span className='icon is-small'>
+                        <i className='fas fa-trash-alt' aria-hidden='true' />
+                    </span>
+                </button>
+            )}
+
+        </div>
+    )
 }
 
 export default App;
--- hello/src/components/Header/index.js
+++ hello/src/components/Header/index.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import '../Header/index.css'
-import Boton from '../Boton'
+import './index.css'
+const Wrapper = props => {
+    return (
 
-const Header = props => {
-    return(
-
-        <header className="App-header">
-            {/* <img src={logo} className="App-logo" alt="logo" /> */}
-            <p>
-                Edit <code>src/App.js</code> and save to reload. <Boton caption='riki'/>
-    </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-    </a>
-        </header>
+        <wrapper className="App-Wrapper">
+            <p>DATA</p>
+        </wrapper>
 
     )
 }
 
-export default Header
+export default Wrapper

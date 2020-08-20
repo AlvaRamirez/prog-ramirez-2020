17a6d9891af1097c91045b616454be8c884dac5a
AlvaRamirez <55244792+AlvaRamirez@users.noreply.github.com>
2020-08-20 15:05:21 -0300

    funciona?

---
+++ artistas/src/components/MainCategory/index.js
@@ -0,0 +1,33 @@
+import React, { useState , useEffect} from 'react'
+import Card from '../Card'
+import axios from 'axios'
+
+const MainCategory = ({id}) => {
+    console.log("hola")
+    const [artist, setArtist] = useState([])
+    
+    useEffect(() => {
+      const fetchData = async ()=> {
+          try{
+              const response = await axios.get('https://artists-api.vercel.app/artists');
+              console.log(response.data);
+              const data= response.data
+              setArtist(data)
+          
+          }catch (error){
+              console.error('este es mi error', error);
+          }
+      }
+      fetchData()
+
+    }, [])
+
+    return (
+        <div className="main">
+          <h1>Lista de Artistas</h1>
+          {artist.map(artist=> <Card key= {artist._id} data={artist}/>)}
+          
+        </div>
+    )
+}
+export default MainCategory
